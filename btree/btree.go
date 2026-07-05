32648dfe2c755aa6ef76954b849415c47188cdc5
Shmadik <necmnou@gmail.com>
2026-07-05 21:07:35 +0300

    add Btree bytes structure

---
+++ btree/btree.go
@@ -0,0 +1,52 @@
+package btree
+
+import (
+	"encoding/binary"
+	"errors"
+)
+
+type BNode []byte
+
+const HEADER = 4
+
+
+// Node представлена как последовательность байтов
+type Btree struct {
+	root_number_page uint64
+	get_node func(uint64) BNode
+	new func(BNode) uint64
+	delete_node func(uint64)
+}
+
+
+func (node BNode) bnode_type() uint16 {
+	return binary.LittleEndian.Uint16(node[0:2])
+}
+
+func (node BNode) bnode_keys_count() uint16 {
+	return binary.LittleEndian.Uint16(node[2:4])
+}
+
+func (node BNode) set_header(type_node uint16, keys_len uint16) {
+	binary.LittleEndian.PutUint16(node[0:2], type_node)
+	binary.LittleEndian.PutUint16(node[2:4], keys_len)
+}
+
+func (node BNode) child_pointer(idx uint16) (uint64, error) {
+	if (idx > node.bnode_keys_count()) {
+		return 0, errors.New("Not match keys")
+	}
+
+	child_ptr := HEADER + idx*8
+	return binary.LittleEndian.Uint64(node[child_ptr:child_ptr+9]), nil
+} 
+
+func (node BNode) set_pointer(idx uint16, ptr uint64)  error {
+	if (idx > node.bnode_keys_count()) {
+		return errors.New("Index ouf of range")
+	}
+	child_ptr := HEADER + idx*8
+	binary.LittleEndian.PutUint64(node[child_ptr:child_ptr+9], ptr)
+}
+
+
