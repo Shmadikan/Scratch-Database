f9b08fea00054626aec0fe8771fa6dd9b622392d
Shmadik <necmnou@gmail.com>
2026-07-14 20:50:39 +0300

    Add Insert into internal node

--- btree/btree.go
+++ btree/btree.go
@@ -173,3 +173,16 @@ func leafInsert(new BNode, old BNode, idx uint16, key []byte, value []byte) {
 	nodeAppendKV(new, idx, 0, key, value)
 	nodeAppendRange(new, old, idx+1, idx, old.bnode_keys_count() - idx)
 }
+
+
+// 
+func NkidInsert(tree *Btree, new BNode, old BNode, idx uint16, kids ...BNode) {
+	kid_count := uint16(len(kids))
+	new.set_header(1, old.bnode_keys_count() + kid_count)
+	nodeAppendRange(new, old, 0, 0, idx)
+	for i, kid := range kids {
+		nodeAppendKV(new, uint16(i)+idx, tree.new(kid), tree.get_node(0), tree.get_node(0))
+	}
+	nodeAppendRange(new, old, idx+kid_count, idx+kid_count-1, old.bnode_keys_count() - (idx + kid_count) - uint16(1))
+}
+
