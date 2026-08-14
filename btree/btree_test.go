ce556671970cecd798325eb9f6ed1593f2a1b1cc
Shmadik <necmnou@gmail.com>
2026-08-14 15:02:26 +0300

    comments and import

--- btree/btree_test.go
+++ btree/btree_test.go
@@ -1,12 +1,14 @@
 package btree
 
 import (
+	"math/rand"
 	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+// Случайный тест
 func TestInsert(t *testing.T) {
 
 	var structure *C = newC()
@@ -25,14 +27,12 @@ func TestInsert(t *testing.T) {
 	assert.Equal(t, structure.get("6"), "123143232", "Not correct key.")
 	assert.Equal(t, structure.get("8"), flowed_value, "Not correct key.")
 	structure.insert("9", flowed_value)
-
-	// Не работает короче нормально сплит, точнее новый рут, содержит неправильные ключи для ссылки на детей, так как там есть сторожевое значение, порядок ключей неправильный и опять же не те ключи ищет
-	// root := structure.tree.get_node(structure.tree.root_number_page)
-	// first_kid,_ := root.get_kidPointer(0)
 	assert.Equal(t, structure.get("9"), flowed_value, "Not correct")
 
 }
 
+
+// Тест с удалением и вставкой
 func TestDelete(t *testing.T) {
 	var structure *C = newC()
 	big_value := strings.Repeat("0", 2500)
@@ -50,7 +50,6 @@ func TestDelete(t *testing.T) {
 	assert.Equal(t, root.bnode_keys_count(), uint16(2), "Not correct")
 	assert.Equal(t, structure.get("2"), "4", "Not correct")
 	structure.insert("9", "test")
-	// фиксануть insert. Что то не так когда идёт вставка в левую ветку
 	structure.insert("10", "huina")
 	
 	assert.Equal(t, structure.get("a"), "huina", "Not correct")
